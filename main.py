309f646bc30b92fc6f08b92b568f97f25e78ed44
freQuensy <mametevalex@gmail.com>
2020-07-20 00:38:55 +0300

    Fixed gitignore

--- main.py
+++ main.py
@@ -9,16 +9,18 @@ import argparse
 
 
 parser = argparse.ArgumentParser(description='Media Powerful Tool')
-parser.add_argument('-filter', action = 'store', dest = "filter", default = None)
+parser.add_argument('-filter', action = 'store', dest = "filter", default = None,
+                    help = 'Available filter names: BLUR CONTOUR DETAIL EDGE_ENHANCE  EDGE_ENHANCE_MORE EMBOSS FIND_EDGES SHARPEN SMOOTH SMOOTH_MORE')
 parser.add_argument('-s', action = 'store', dest = 'res_filename', default = None)
 parser.add_argument('-crop', action = 'store', dest = 'crop', default = None)
 parser.add_argument("-a",action = 'store', dest = "angle", default = None)
+parser.add_argument('input_filename', action = 'store', help = 'Image file to work with')
 
 
 def main():
     args = parser.parse_args()
     print(args)
-
+    if
 
 if __name__ == '__main__':
     main()
